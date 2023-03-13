2e2b616f604520c12ed3accf636370e6b6aa2df1
omkarjadhav111 <123991131+omkarjadhav111@users.noreply.github.com>
2023-03-13 18:49:24 +0530

    Greater number between three number in java

---
+++ Greater_Number.java
@@ -0,0 +1,17 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    int a=20,b=100,c=100;
+	    if(a>b&&a>c)
+	    {
+	        System.out.println("the a is greter");
+	    } 
+	    else if (b>a&&b>c)
+	    {
+	        System.out.println("the b is greter");
+	    }
+	    else{
+	        System.out.println("The c is greter");
+	    }
+	}
+}
