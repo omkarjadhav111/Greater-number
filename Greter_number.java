e7b6bd47dbfe2c450d4e969944e71601facc7468
omkarjadhav111 <123991131+omkarjadhav111@users.noreply.github.com>
2023-03-13 18:44:31 +0530

    greater number between two number in java

---
+++ Greter_number.java
@@ -0,0 +1,16 @@
+public class Main
+{
+	public static void main(String[] args) {
+		int a= 10, b=20;
+		if(a>b)
+		{
+		    System.out.println("a is greter");
+		    
+		}
+		else
+		{
+		    System.out.println("b is greter");
+		}
+		
+	}	
+}
